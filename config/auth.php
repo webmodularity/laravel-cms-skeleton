c42a49deb926949fb5ce0e2aa30276d0f4563718
Rory Hellyer <rory@webmodularity.com>
2017-02-17 17:20:08 -0800

    docs

--- config/auth.php
+++ config/auth.php
@@ -66,8 +66,8 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => App\User::class,
+            'driver' => 'wm-eloquent',
+            'model' => WebModularity\LaravelAuth\User\User::class,
         ],
 
         // 'users' => [
